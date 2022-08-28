c6becd43d096d89c2244135d2087d4101cbff557
yashaskaram <contactvikas24@gmail.com>
2022-08-28 17:13:18 -0500

    _ViewImports, ViewBag, ViewData

--- BookStore_App/BookStore_App/Controllers/HomeController.cs
+++ BookStore_App/BookStore_App/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore_App.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,18 +10,27 @@ namespace BookStore_App.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string Title { get; set; }
+        [ViewData]
+        public BookModel Book { get; set; }
         public ViewResult Index()
         {
+            Title = "Home";
+            Book = new BookModel() { Id = 1, Author = "Mr Writer" };
+
             return View();
         }
 
         public ViewResult AboutUs()
         {
+            Title = "About us";
             return View();
         }
 
         public ViewResult ContactUs()
         {
+            Title = "Contact us";
             return View();
         }
     }
